b87a9dc4dcaae3d775ced9c4f42f1789ded8a61e
Slinky <lroberts28@murraystate.edu>
2024-12-01 14:08:20 -0600

    created admin

--- StudentEnrollmentSystem.py
+++ StudentEnrollmentSystem.py
@@ -54,14 +54,6 @@ def create_db():
                     FOREIGN KEY(enrollment_id) REFERENCES enrollments(enrollment_id)
                   )''')
     
-    c.execute('''CREATE TABLE IF NOT EXISTS admin_users (
-                    admin_id INTEGER PRIMARY KEY,
-                    first_name TEXT,
-                    last_name TEXT,
-                    email TEXT UNIQUE,
-                    password TEXT
-                  )''')
-    
     conn.commit()
     conn.close()
 
